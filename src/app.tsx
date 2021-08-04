4cff8763544972442dcb525c5142d35e1d729e36
Dasha1801 <6227968@gmail.com>
2021-08-04 23:20:57 +0300

    feat: add component Form, validate form.

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+import {Form} from './form/form';
+
+
+export function App():JSX.Element{
+  // const [setFormValues] = useState([]);
+  return(
+    // <Form setFormValues={setFormValues}/>
+    <Form/>
+  );
+}
---
+++ src/form/form.tsx
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from 'react';
+import './form.css';
+
+export const Form = ({setFormValues}) : JSX.Element=> {
+
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [birthDate, setBirthDate] = useState('');
+  const [country, setCountry] = useState('UK');
+  const [agree, setAgree] = useState(false);
+  const [errors, setErrors] = useState({});
+
+  const validate = () => {
+    setErrors({});
+    if(!agree){
+      setErrors((state) => ({...state, agree}))
+    }
+    if(firstName === ''){
+      setErrors((state) => ({...state, firstName}))
+    }
+    if(lastName === ''){
+      setErrors((state) => ({...state, lastName}))
+    }
+    if(birthDate === ''){
+      setErrors((state) => ({...state, birthDate}))
+    }
+    if(country === ''){
+      setErrors((state) => ({...state, country}))
+    }
+  }
+
+  useEffect(() => {
+    validate();
+  }, [firstName, lastName, birthDate, country, agree])
+
+  const handlerSubmit = (event:React.SyntheticEvent) => {
+    event.preventDefault();
+    if(Object.keys(errors).length === 0){
+      setFormValues((state:any) => [...state, {firstName, lastName, birthDate, country, agree}]);
+      reset();
+    }
+  }
+
+
+  const reset = () => {
+    setFirstName('');
+    setLastName('');
+    setBirthDate('');
+    setAgree(false);
+    setCountry('UK');
+  }
+
+  return (
+    <form onSubmit = {handlerSubmit} className='formStyle'>
+      <label htmlFor='firstName'>
+        <p>Name: {errors?.firstName === '' && <span>* name should be fill</span>}</p>
+        <input type='text'
+        name='firstName'
+        value={firstName}
+        onChange={(event)=> setFirstName(event.target.value)}/>
+      </label>
+      <label htmlFor='lastName'>
+        <p>Surname: {errors?.lastName === '' && <span>* surname should be fill</span>}</p>
+        <input type='text'
+        name='lastName'
+        value={lastName}
+        onChange={(event)=> setLastName(event.target.value)}/>
+      </label>
+      <label htmlFor='birthDate'>
+      <p>Birth date: {errors?.birthDate === '' && <span>* birth date should be fill</span>}</p>
+        <input type='date'
+        name='birthDate'
+        value={birthDate}
+        onChange={(event)=> setBirthDate(event.target.value)}/>
+      </label>
+      <label htmlFor='country'>
+      <p>Country: {errors?.country === '' && <span>* country should be fill</span>}</p>
+        <select className='' name='country' value={country} onChange={(event)=> setCountry(event.target.value)}>
+          <option>Russia</option>
+          <option>Belarus</option>
+          <option>UK</option>
+          <option>USA</option>
+        </select>
+      </label >
+      <label htmlFor='agree'>
+        <p>This box I agree...{errors?.agree !== undefined && <span>* agree should be check</span>}</p>
+          <input type='checkbox' name='agree' checked={agree} onChange={() => setAgree(prev => !prev)}/>
+      </label>
+      <div>
+        <input type='submit' value='Send' className='btn'/>
+      </div>
+    </form>
+  );
+}
