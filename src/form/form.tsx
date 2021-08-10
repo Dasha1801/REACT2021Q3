f17ea082a07f1494585c4069cfb3d4dc0e165038
Dasha1801 <6227968@gmail.com>
2021-08-10 11:41:59 +0300

    feat: add dispatch message

--- src/form/form.tsx
+++ src/form/form.tsx
@@ -7,12 +7,18 @@ interface Errors {
   lastName?: string;
   country?: string;
   birthDate?: string;
-  gender?: string;
+  gender?: PossibleSex;
   agree?: boolean;
 }
 
 interface IForm {
   setFormValues: Dispatch<SetStateAction<formPropType[]>>;
+}
+
+export enum PossibleSex{
+  male = 'male',
+  female = 'female',
+  empty = ''
 }
 
 export const Form: React.FC<IForm>= ({setFormValues}): JSX.Element => {
@@ -22,7 +28,7 @@ export const Form: React.FC<IForm>= ({setFormValues}): JSX.Element => {
   const [birthDate, setBirthDate] = useState('');
   const [country, setCountry] = useState('UK');
   const [agree, setAgree] = useState(false);
-  const [gender, setGender] = useState('Male');
+  const [gender, setGender] = useState(PossibleSex.empty);
   const [errors, setErrors] = useState({}as Errors);
 
   const validate = () => {
@@ -42,20 +48,24 @@ export const Form: React.FC<IForm>= ({setFormValues}): JSX.Element => {
     if(country === ''){
       setErrors((state) => ({...state, country}))
     }
+    if(gender === PossibleSex.empty){
+      setErrors((state) => ({...state, gender}))
+    }
   }
 
   useEffect(() => {
     validate();
-  }, [firstName, lastName, birthDate, country, agree, gender])
+  }, [firstName, lastName, birthDate, country, agree, gender]);
+
 
   const handlerSubmit = (event:React.SyntheticEvent) => {
     event.preventDefault();
     if(Object.keys(errors).length === 0){
+      alert(`${firstName}, thank you for visiting our site, all data has been successfully saved!`);
       setFormValues((state:formPropType[]) => [...state, {firstName, lastName, birthDate, country, agree, gender}]);
       reset();
     }
   }
-
 
   const reset = () => {
     setFirstName('');
@@ -63,6 +73,7 @@ export const Form: React.FC<IForm>= ({setFormValues}): JSX.Element => {
     setBirthDate('');
     setAgree(false);
     setCountry('UK');
+    setGender(PossibleSex.empty);
   }
 
   return (
@@ -99,15 +110,15 @@ export const Form: React.FC<IForm>= ({setFormValues}): JSX.Element => {
       </label >
       <label htmlFor='agree' className='checkBox label'>
       <input type='checkbox' name='agree' checked={agree} onChange={() => setAgree(prev => !prev)}/>
-        <p className='labelName'>Use of this website constitutes acceptance of the Terms of Use and Privacy Policy. {errors?.agree !== undefined && <span className='warning'>* agree should be check</span>}</p>
+        <p className='labelCheckbox'>Use of this website constitutes acceptance of the Terms of Use and Privacy Policy. {errors?.agree !== undefined && <span className='warning'>* agree should be check</span>}</p>
       </label>
-
       <label htmlFor='gender' className='gender label'>
-      <p className='labelName'>Male: <input type='radio' name='gender' checked onChange={() => setGender('Male')}/></p>
-      <p className='labelName'>Female: <input type='radio' name='gender' onChange={() => setGender('Female')}/></p>
+        <p>Specify gender: {errors?.gender === PossibleSex.empty && <span className='warning'>* choose one of the options</span>}</p>
+        <div className='wrapper'>
+          <p className='labelGender'>Male: <input type='radio'  name='gender' checked= {gender===PossibleSex.male} onChange={() => setGender(PossibleSex.male)}/></p>
+          <p className='labelGender'>Female: <input type='radio' name='gender' checked= {gender===PossibleSex.female} onChange={() => setGender(PossibleSex.female)}/></p>
+        </div>
       </label>
-
-
       <div>
         <input type='submit' value='Send' className='btn'/>
       </div>
--- src/shared/formPropType.ts
+++ src/shared/formPropType.ts
@@ -1,7 +1,9 @@
+import { PossibleSex } from "../form/form";
+
 export type formPropType = {
   firstName: string;
   lastName: string;
   country: string;
   birthDate: string;
-  gender: string;
+  gender: PossibleSex;
 }
