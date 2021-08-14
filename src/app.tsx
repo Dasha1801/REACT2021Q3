ab4f04be8a3b8f6bb13b82696d8f83702d7724d6
Dasha1801 <6227968@gmail.com>
2021-08-14 10:19:33 +0300

    feat: add about page, notFound page, router.

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,33 @@
 import React from 'react';
 import { Search } from './components/search/search';
-// import ReactDOM from 'react-dom';
-// import './style.css';
+import {
+  Route,
+  NavLink,
+  HashRouter,
+  Switch
+} from 'react-router-dom';
+import About from './components/about/about';
+import NotFound from './components/notFound/notFound';
+// import './app.css';
 
 
 const App = () => {
   return (
-    <>
-    <Search/>
-    </>
+    <HashRouter>
+      <nav>
+        <ul>
+          <li><NavLink exact to='/'>Home</NavLink></li>
+          <li><NavLink to='/about'>About</NavLink></li>
+        </ul>
+      </nav>
+      <div className='content'>
+        <Switch>
+          <Route exact path='/' component={Search}/>
+          <Route path='/about' component={About}/>
+          <Route path='/*' component={NotFound}/>
+        </Switch>
+      </div>
+    </HashRouter>
   )
 }
 
---
+++ src/components/about/about.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const About = () => {
+  return <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi ullam labore minus eligendi veniam error laborum iste. Ullam eaque enim impedit, laboriosam quas adipisci odit est hic ipsa at, qui veniam, provident commodi eos!</div>
+}
+
+export default About;
