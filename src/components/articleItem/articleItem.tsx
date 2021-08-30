39ab4cba898a4752280aae77bfebbe49247bab24
Dasha1801 <6227968@gmail.com>
2021-08-30 11:15:34 +0300

    feat: add test component article item

--- src/components/articleItem/articleItem.tsx
+++ src/components/articleItem/articleItem.tsx
@@ -6,18 +6,19 @@ interface InfoItem{
   info: ArticleInfo
 }
 
-export const ArticleItem:React.FC<InfoItem>= ({info}, index) => {
+export const ArticleItem:React.FC<InfoItem>= ({info}) => {
 
   return(
-    <div className='container-item' key={index}>
+    <>
+    <div className='container-item'>
       <div>
           <h4 className='titleH4'>{info.title}</h4>
           <h5 className='titleH5'>Author: {info.author}</h5>
           <h5 className='titleH5'>{info.publishedAt}</h5>
           <p className='text'><span className='titleText'>Description:</span> {info.description}</p>
       </div>
-      <div> <img src={info.urlToImage} className='image'></img></div>
+      <div> <img src={info.urlToImage} className='image' data-testid='img-url'></img></div>
     </div>
+    </>
   )
 }
-
