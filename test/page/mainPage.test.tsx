fa34378bca3ed799428427d7db1c57ba481fb88a
Dasha1801 <6227968@gmail.com>
2021-08-30 16:15:30 +0300

    feat: add test links on Main page

---
+++ test/page/mainPage.test.tsx
@@ -0,0 +1,13 @@
+import {render} from '@testing-library/react';
+import { Provider } from 'react-redux';
+import App from '../../src/components/app/app';
+import React from 'react';
+import { store } from '../../src/redux/store';
+
+it('Link text on Main page', () => {
+  const el = render(<Provider store={store}> <App/> </Provider>);
+  const elLinkHome = el.getByText('Home');
+  const elLinkAbout = el.getByText('About');
+  expect(elLinkHome.textContent).toBe('Home');
+  expect(elLinkAbout.textContent).toBe('About');
+ });
